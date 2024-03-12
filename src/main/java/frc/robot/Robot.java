f169df25b485854aecb3fec5f9d6552fc0fd4237
CoronaFetish <160544589+CoronaFetish@users.noreply.github.com>
2024-03-12 14:55:38 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,38 +4,130 @@
 
 package frc.robot;
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import com.revrobotics.CANSparkMax;
-//import edu.wpi.first.wpilibj.XboxController;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private static final XboxController xbox1 = new XboxController(0);
+  private static final Drive m_drive = new Drive();
+  private static final Shooter m_shooter = new Shooter();
+  //private static final autonomousTest m_autoTest = new autonomousTest();
 
 
 
-/** Add your docs here. */
-public class Shooter {
-    private final CANSparkMax shooterMotor = new CANSparkMax(Constants.shoot, MotorType.kBrushless);
-   public void ShooterBringRing(boolean getBButtonPressed) {
-      if (getBButtonPressed) {
-         shooterMotor.set(-0.1);
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {}
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+ 
+  private Timer autoTimer = new Timer();        // makes timer named autoTimer
+  @Override
+  public void autonomousInit() {
+    autoTimer.reset();
+    autoTimer.start();
+  }
+
+  /** This function is called periodically during autonomous. */
+  
+    @Override
+    public void autonomousPeriodic() {
+      if (autoTimer.get() < 5.0) {
+        m_drive.drive.arcadeDrive(-0.5, 0); // for first five seconds go forward at half speed
+      }
+      else if (autoTimer.get() < 7.0) {
+        m_drive.drive.arcadeDrive(0, -0.5); // for next two seconds turn right at half speed (just cause it can)
+      }
+      else if (autoTimer.get() < 9.0) {
+        m_drive.drive.arcadeDrive(0, 0.5); // for nest two seconds turn left at half speed
       }
       else {
-         shooterMotor.set(0);
+        m_drive.drive.arcadeDrive(0, 0); // if nothing requires the timer then stop
       }
-   }
-   public void shooterOnHalf(boolean getXButtonPressed) {
-      if (getXButtonPressed) {
-         shooterMotor.set(0.5);
-      }
-      else {
-         shooterMotor.set(0);
-      }
-   }
+  } 
 
-   public void shooterOn(boolean getAButtonPressed) {
-      if (getAButtonPressed ) {
-         shooterMotor.set(1);//sets shooter to full speed
-      } else {
-         shooterMotor.set(0);//sets shooter to no speed
-      }
-      
-   }
+
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    m_drive.drive(xbox1.getLeftY(), xbox1.getRightX());
+    m_shooter.shooterOn(xbox1.getAButtonPressed());
+    m_shooter.shooterOnHalf(xbox1.getXButtonPressed());
+    m_shooter.ShooterBringRing(xbox1.getBButtonPressed());
+   // m_autoTest.autoTest(xbox1.getXButtonPressed());
+      //lets xbox controller be used left sick up and down right for turning and A for shoot
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is  called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
 }
