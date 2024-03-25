291f4e83cee2374f231a72423d24224752f35763
CoronaFetish <160544589+CoronaFetish@users.noreply.github.com>
2024-03-25 14:37:30 -0500

    Drive.java

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -4,16 +4,9 @@
 
 package frc.robot;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-
-import edu.wpi.first.hal.CANAPITypes.CANDeviceType;
-import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
 
 /** Add your docs here. */
 public class Drive {
@@ -33,17 +26,12 @@ public class Drive {
     private final CANSparkMax lR = new CANSparkMax(Constants.lRMotor, MotorType.kBrushed); //left rear motor
     private final CANSparkMax rF = new CANSparkMax(Constants.rFMotor, MotorType.kBrushed); //right front motor
     private final CANSparkMax rR = new CANSparkMax(Constants.rRMotor, MotorType.kBrushed); //right rear motor
-
-
-    // private final Motor
-
-    public final DifferentialDrive drive = new DifferentialDrive(lR, rR); // 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public final DifferentialDrive drive = new DifferentialDrive(lR, rR);
 
     void drive(double throttle, double rotation) {
         lF.follow(lR);
         rF.follow(rR);
-        drive.arcadeDrive(throttle, rotation);
+        drive.arcadeDrive(-throttle, rotation);
     }
-
-
 }
